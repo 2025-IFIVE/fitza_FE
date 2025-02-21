237e1b4770ff166c50ba09354e38e220505a3375
minhye <doreminye@gmail.com>
2025-02-21 16:19:51 +0900

    mypage_modify

---
+++ fitza_fe/src/pages/Mypage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import * as MP from "../styles/MypageStyle";
+
+function Mypage() {
+    return (
+        <>
+            <Header />
+            <MP.Main>
+                여기는 마이페이지
+            </MP.Main>
+            <Footer />
+        </>
+    );
+}
+
+export default Mypage;
