a4e98fc5db159964189fce714c1f9c4251eb5b5f
minhye <doreminye@gmail.com>
2025-05-01 23:28:20 +0900

    개인 정보 페이지 초기화

--- fitza_fe/src/App.js
+++ fitza_fe/src/App.js
@@ -13,6 +13,7 @@ import MyCloset_1 from "./pages/MyCloset_1";
 import MyCloset_2 from "./pages/MyCloset_2";
 import MyCloset_3 from "./pages/MyCloset_3";
 import Mypage from "./pages/Mypage";
+import Person from "./pages/Person";
 import CalendarPage from "./pages/CalendarPage";
 import ShareCloset from "./pages/ShareCloset";
 import Friends from "./pages/Friends";
@@ -25,7 +26,7 @@ function App() {
   return (
     <div className="App">
       <EnglishFontStyle /> {/* 여기에서 폰트 스타일 적용 */}
-      <ScrollToTop /> 
+      <ScrollToTop />
       <Routes>
         <Route exact path="/main" element={<Main />} />
         <Route exact path="/" element={<Splash />} />
@@ -35,6 +36,7 @@ function App() {
         <Route path="/category/:categoryName" element={<MyCloset_2 />} />
         <Route exact path="/mycloset_3" element={<MyCloset_3 />} />
         <Route exact path="/mypage" element={<Mypage />} />
+        <Route exact path="/person" element={<Person />} />
         <Route exact path="/calendarpage" element={<CalendarPage />} />
         <Route exact path="/sharecloset" element={<ShareCloset />} />
         <Route exact path="/friends" element={<Friends />} />
