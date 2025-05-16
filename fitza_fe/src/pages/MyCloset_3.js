a98b62f5a3b3c7e091f1dcbd28b7107cfe2a2dfd
sumin <sumin990103@naver.com>
2025-05-16 11:28:14 +0900

    하는중

--- fitza_fe/src/pages/MyCloset_3.js
+++ fitza_fe/src/pages/MyCloset_3.js
@@ -54,10 +54,10 @@ function MyCloset_3() {
 
 
   const categoryData = {
-    상의: ["티셔츠", "긴팔티", "민소매티", "카라티", "탱크탑", "크롭탑", "블라우스", "셔츠", "맨투맨", "후드", "니트", "조끼", "가디건", "스포츠", "기타"],
-    하의: ["청바지", "슬랙스", "반바지", "트레이닝바지", "레깅스", "미니스커트","미디스커트","롱스커트","기타"],
-    아우터: ["코트", "패딩", "가디건", "무스탕","블루종","야구잠바","자켓","조끼","집업","야상","라이더자켓","후리스","경량패딩","기타"],
-    원피스: ["캐주얼원피스", "미니원피스", "티셔츠원피스", "셔츠원피스", "니트원피스","후드원피스","자켓원피스","멜빵원피스","점프슈트","파티원피스","기타"],
+    상의: ["탑", "블라우스", "티셔츠", "니트웨어", "셔츠", "브라탑", "후드티"],
+    하의: ["청바지", "팬츠", "스커트", "레깅스", "조거팬츠"],
+    아우터: ["코트", "재킷", "점퍼", "패딩","베스트","가디건","짚업"],
+    원피스: ["드레스","점프수트"],
     신발: ["스니커즈", "슬립온", "운동화", "로퍼", "플랫슈즈","힐","샌들","샌들힐","슬리퍼","뮬","부츠","워커","어그부츠","등산화","기타"],
     모자: ["캡", "비니", "베레모", "페도라", "기타"],
     기타: ["홈웨어", "헤어악세사리", "안경/선글라스", "스카프", "목도리","시계","장갑","양말","벨트","지갑","기타"]
@@ -67,37 +67,66 @@ function MyCloset_3() {
   const [selectedSubCtgy, setSelectedSubCtgy] = useState([]);
   const [activeCategory, setActiveCategory] = useState(null);
 
-  const [selectedSeasons, setSelectedSeasons] = useState([]);
-  const seasons = ["봄", "여름", "가을", "겨울"];
-
-  const [selectedTPO, setSelectedTPO] = useState([]);
-  const TPO = ["데일리", "직장", "데이트", "경조사","여행","홈웨어","운동","학교","특별한 날","기타"];
+  const [selectedDetail, setSelectedDetail] = useState([]);
+  const detail = ["메탈", "리본/띠", "봉제/자수", "허리라인","절개/비대칭","단추","지퍼","스트링","소매/밑단","포켓","주름","어깨라인","레이스","기타"];
 
   const [selectedcolor, setSelectedcolor] = useState([]);
-  const color = ["흰색", "아이보리", "베이지", "연회색","진회색","검정","연노랑","노랑","황색","주황","코랄","빨강","분홍","진분홍","연두","초록","올리브","다크올리브","청록","카키","하늘","파랑","네이비","라벤더","보라","버건디","카멜","브라운","다크브라운","마젠타","골드","실버","기타"];
+  const color = ["블랙", "화이트", "그레이", "레드", "핑크", "오렌지", "베이지", "브라운", "옐로우", "그린", "카키", "민트", "블루", "네이비", "스카이블루", "퍼플", "라벤더", "와인", "네온", "골드", "실버"];
 
   const [selectedCloth, setSelectedCloth] = useState([]);
-  const cloth = ["면", "폴리에스터", "나일론", "데님","가죽","울","린넨","아크릴","실크","트위드","벨벳","쉬폰","스웨이드","코듀로이","레이스","퍼","다운 깃털","캐시미어","앙고라","기타"];
+  const cloth = ["패딩", "퍼", "니트", "울/플리스","골덴/벨벳","트위드","가죽","데님","소프트 패브릭","린넨","면","신축/기능성 소재","기타"];
 
-  const [selectedPattern, setSelectedPattern] = useState([]);
-  const pattern = ["무지", "스트라이프", "프린트", "도트","애니멀","플로럴","트로피칼","아가일","카모","컬러블록","체커보드","격자","하운드투스","헤링본","쉐브론","트위드","타이다이","레이스","기타"];
+  const [selectedPrint, setSelectedPrint] = useState([]);
+  const print = ["무지", "레터링", "체크", "스트라이프","기하학/반복 패턴","도트","자연/동물무늬","추상/그래픽","밀리터리 패턴","기타"];
 
   const [selectedStyle, setSelectedStyle] = useState([]);
-  const style = ["캐주얼", "비즈니스 캐주얼", "포멀", "모던","클래식","미니멀","럭셔리","스포티","트렌디","키드코어","아티스틱","드레스업","페미닌","스트릿","기타"];
+  const style = ["스트리트", "힙합", "페미닌", "모던/클래식","레트로/빈티지","밀리터리","스포티","로맨틱","리조트","기타"];
+
+  const [selectedSubStyle, setSelectedSubStyle] = useState([]);
+  const substyle = ["스트리트", "힙합", "페미닌", "모던/클래식","레트로/빈티지","밀리터리","스포티","로맨틱","리조트","기타"];
 
   const [selectedFit, setSelectedFit] = useState([]);
-  const fit = ["슬림", "레귤러", "루즈", "오버사이즈"];
+  const fit = ["슬림","타이트", "레귤러", "루즈", "오버사이즈"];
+
+  const [selectedbottomFit, setSelectedbottomFit] = useState([]);
+  const bottomfit = ["스키니", "노멀", "와이드", "루즈","벨보텀"];
 
   const [selectedNeckline, setSelectedNeckline] = useState([]);
-  const neckline = ["라운드넥", "U넥", "V넥", "스퉤어넥","셔츠칼라","스탠드업 칼라","와이드칼라","터틀넥","스트랩 없음","두꺼운 스트랩","얇은 스트랩","오프숄더","비대칭넥","훌터넥","수트칼라","기타"];
+  const neckline = ["라운드넥", "브이넥", "스퀘어넥","오프숄더","터틀넥","노카라","기타"];
 
   const [selectedSleeve, setSelectedSleeve] = useState([]);
   const sleeve = ["민소매","캡소매","반소매","7부 소매","긴소매"];
 
   const [selectedLength, setSelectedLength] = useState([]);
-  const length = ["미니","미디움","맥시"];
+  const length = ["크롭","노멀","롱"];
 
-  const [purchaseDate, setPurchaseDate] = useState(""); 
+  const [selectedbottomLength, setSelectedbottomLength] = useState([]);
+  const bottomlength = ["미니","니렝스","미디","발목","맥시"];
+
+  const [selectedouterLength, setSelectedouterLength] = useState([]);
+  const outerlength = ["크롭","노멀","하프","맥시","롱"];
+
+  const [selecteddressLength, setSelecteddressLength] = useState([]);
+  const dresslength = ["미니","니렝스","미디","발목","맥시"];
+
+  const getFitOptions = () => {
+    switch (selectedCtgy) {
+      case "하의":
+        return { options: bottomfit, selected: selectedbottomFit, setSelected: setSelectedbottomFit };
+      case "상의":
+      case "원피스":
+      case "아우터":
+        return { options: fit, selected: selectedFit, setSelected: setSelectedFit };
+      default:
+        return { options: [], selected: [], setSelected: () => {} };
+    }
+  };
+
+  const handleFitClickDynamic = (item) => {
+    const { setSelected } = getFitOptions();
+    setSelected([item]);   // ⭐ 단일 선택
+  };
+  
 
 
   const handleEditClick = () => {
@@ -123,23 +152,15 @@ function MyCloset_3() {
   };
   
   
-  const handleSubCtgyClick = (subcategory) => {
-    if (selectedSubCtgy.includes(subcategory)) {
-      setSelectedSubCtgy(selectedSubCtgy.filter((sc) => sc !== subcategory));
-    } else {
-      setSelectedSubCtgy([...selectedSubCtgy, subcategory]);
-    }
-  };
+  // ⭐ 변경: 하위 카테고리 (단일 선택으로 수정)
+const handleSubCtgyClick = (subcategory) => {
+  setSelectedSubCtgy([subcategory]);
+};
 
-  const handleSeasonClick = (season) => {
-    setSelectedSeasons((prev) =>
-      prev.includes(season) ? prev.filter((s) => s !== season) : [...prev, season]
-    );
-  };
 
-  const handleTPOClick = (TPO) => {
-    setSelectedTPO((prev) =>
-      prev.includes(TPO) ? prev.filter((s) => s !== TPO) : [...prev, TPO]
+  const handleDetailClick = (detail) => {
+    setSelectedDetail((prev) =>
+      prev.includes(detail) ? prev.filter((s) => s !== detail) : [...prev, detail]
     );
   };
 
@@ -155,50 +176,55 @@ function MyCloset_3() {
     );
   };
 
-  const handlePatternClick = (pattern) => {
-    setSelectedPattern((prev) =>
-      prev.includes(pattern) ? prev.filter((s) => s !== pattern) : [...prev, pattern]
+  const handlePrintClick = (print) => {
+    setSelectedPrint((prev) =>
+      prev.includes(print) ? prev.filter((s) => s !== print) : [...prev, print]
     );
   };
 
-  const handleStyleClick = (style) => {
-    setSelectedStyle((prev) =>
-      prev.includes(style) ? prev.filter((s) => s !== style) : [...prev, style]
-    );
-  };
+  // ⭐ 변경: 스타일
+const handleStyleClick = (item) => handleSingleSelect(selectedStyle, setSelectedStyle, item);
 
-  const handleFitClick = (fit) => {
-    setSelectedFit((prev) =>
-      prev.includes(fit) ? prev.filter((s) => s !== fit) : [...prev, fit]
-    );
-  };
+// ⭐ 변경: 서브스타일
+const handleSubStyleClick = (item) => handleSingleSelect(selectedSubStyle, setSelectedSubStyle, item);
 
-  const handleNecklineClick = (neckline) => {
-    setSelectedNeckline((prev) =>
-      prev.includes(neckline) ? prev.filter((s) => s !== neckline) : [...prev, neckline]
-    );
-  };
 
-  const handleSleeveClick = (sleeve) => {
-    setSelectedSleeve((prev) =>
-      prev.includes(sleeve) ? prev.filter((s) => s !== sleeve) : [...prev, sleeve]
-    );
-  };
+  // ⭐ 변경: 넥라인 (단일 선택으로 수정)
+const handleNecklineClick = (item) => handleSingleSelect(selectedNeckline, setSelectedNeckline, item);
+  // ⭐ 변경: 소매길이
+const handleSleeveClick = (item) => handleSingleSelect(selectedSleeve, setSelectedSleeve, item);
 
-  const handleLengthClick = (length) => {
-    setSelectedLength((prev) =>
-      prev.includes(length) ? prev.filter((s) => s !== length) : [...prev, length]
-    );
-  };
+  // ✅ 상의/하의/아우터/원피스에 따라 length 값을 동적으로 반환
+const getLengthOptions = () => {
+  switch (selectedCtgy) {
+    case "상의":
+      return { options: length, selected: selectedLength, setSelected: setSelectedLength };
+    case "하의":
+      return { options: bottomlength, selected: selectedbottomLength, setSelected: setSelectedbottomLength };
+    case "아우터":
+      return { options: outerlength, selected: selectedouterLength, setSelected: setSelectedouterLength };
+    case "원피스":
+      return { options: dresslength, selected: selecteddressLength, setSelected: setSelecteddressLength };
+    default:
+      return { options: [], selected: [], setSelected: () => {} };
+  }
+};
 
-  const handleDateChange = (event) => {
-    setPurchaseDate(event.target.value);
-  };
-  
+// ✅ handle 함수 (공통화 가능)
+const handleLengthClickDynamic = (item) => {
+  const { setSelected } = getLengthOptions();
+  setSelected([item]);   // ⭐ 단일 선택
+};
+
+const handleSingleSelect = (selected, setSelected, item) => {
+  setSelected([item]);
+};
 
   const handleBackButtonClick = () => {
     navigate(-1); // 이전 페이지로 이동
   };
+
+ 
 
   return (
     <M.Background>
@@ -309,24 +335,6 @@ function MyCloset_3() {
               </M.SubCategoryItems>
             )}
 
-  <M.CategoryTitle>계절</M.CategoryTitle>
-  <M.CategoryItems>
-    {isEditing
-      ? seasons.map((season) => (
-          <M.CategoryItem
-            key={season}
-            selected={selectedSeasons.includes(season)}
-            onClick={() => handleSeasonClick(season)}
-          >
-            {season}
-          </M.CategoryItem>
-        ))
-      : selectedSeasons.map((season) => (
-          <M.CategoryItem key={season} selected>
-            {season}
-          </M.CategoryItem>
-        ))}
-  </M.CategoryItems>
   <M.CategoryTitle>색상</M.CategoryTitle>
   <M.CategoryItems>
     {isEditing
@@ -345,21 +353,21 @@ function MyCloset_3() {
           </M.CategoryItem>
         ))}
   </M.CategoryItems>
-  <M.CategoryTitle>TPO</M.CategoryTitle>
+  <M.CategoryTitle>디테일</M.CategoryTitle>
   <M.CategoryItems>
     {isEditing
-      ? TPO.map((tpo) => (
+      ? detail.map((detail) => (
           <M.CategoryItem
-            key={tpo}
-            selected={selectedTPO.includes(tpo)}
-            onClick={() => handleTPOClick(tpo)}
+            key={detail}
+            selected={selectedDetail.includes(detail)}
+            onClick={() => handleDetailClick(detail)}
           >
-            {tpo}
+            {detail}
           </M.CategoryItem>
         ))
-      : selectedTPO.map((tpo) => (
-          <M.CategoryItem key={tpo} selected>
-            {tpo}
+      : selectedDetail.map((detail) => (
+          <M.CategoryItem key={detail} selected>
+            {detail}
           </M.CategoryItem>
         ))}
   </M.CategoryItems>
@@ -381,19 +389,19 @@ function MyCloset_3() {
           </M.CategoryItem>
         ))}
   </M.CategoryItems>
-  <M.CategoryTitle>패턴</M.CategoryTitle>
+  <M.CategoryTitle>프린트</M.CategoryTitle>
   <M.CategoryItems>
     {isEditing
-      ? pattern.map((item) => (
+      ? print.map((item) => (
           <M.CategoryItem
             key={item}
-            selected={selectedPattern.includes(item)}
-            onClick={() => handlePatternClick(item)}
+            selected={selectedPrint.includes(item)}
+            onClick={() => handlePrintClick(item)}
           >
             {item}
           </M.CategoryItem>
         ))
-      : selectedPattern.map((item) => (
+      : selectedPrint.map((item) => (
           <M.CategoryItem key={item} selected>
             {item}
           </M.CategoryItem>
@@ -417,89 +425,117 @@ function MyCloset_3() {
           </M.CategoryItem>
         ))}
   </M.CategoryItems>
+  <M.CategoryTitle>서브 스타일</M.CategoryTitle>
+  <M.CategoryItems>
+    {isEditing
+      ? substyle.map((item) => (
+          <M.CategoryItem
+            key={item}
+            selected={selectedSubStyle.includes(item)}
+            onClick={() => handleSubStyleClick(item)}
+          >
+            {item}
+          </M.CategoryItem>
+        ))
+      : selectedSubStyle.map((item) => (
+          <M.CategoryItem key={item} selected>
+            {item}
+          </M.CategoryItem>
+        ))}
+  </M.CategoryItems>
   <M.CategoryTitle>핏</M.CategoryTitle>
-  <M.CategoryItems>
-    {isEditing
-      ? fit.map((item) => (
+<M.CategoryItems>
+  {(() => {
+    const { options, selected } = getFitOptions();
+    return isEditing
+      ? options.map((item) => (
           <M.CategoryItem
             key={item}
-            selected={selectedFit.includes(item)}
-            onClick={() => handleFitClick(item)}
+            selected={selected.includes(item)}
+            onClick={() => handleFitClickDynamic(item)}
           >
             {item}
           </M.CategoryItem>
         ))
-      : selectedFit.map((item) => (
+      : selected.map((item) => (
           <M.CategoryItem key={item} selected>
             {item}
           </M.CategoryItem>
-        ))}
-  </M.CategoryItems>
-  <M.CategoryTitle>넥라인</M.CategoryTitle>
-  <M.CategoryItems>
-    {isEditing
-      ? neckline.map((item) => (
+        ));
+  })()}
+</M.CategoryItems>
+
+  {/* 넥라인 : 하의가 아닐 때만 표시 */}
+{selectedCtgy !== "하의" && (
+  <>
+    <M.CategoryTitle>넥라인</M.CategoryTitle>
+    <M.CategoryItems>
+      {isEditing
+        ? neckline.map((item) => (
+            <M.CategoryItem
+              key={item}
+              selected={selectedNeckline.includes(item)}
+              onClick={() => handleNecklineClick(item)}
+            >
+              {item}
+            </M.CategoryItem>
+          ))
+        : selectedNeckline.map((item) => (
+            <M.CategoryItem key={item} selected>
+              {item}
+            </M.CategoryItem>
+          ))}
+    </M.CategoryItems>
+  </>
+)}
+
+{/* 소매길이 : 하의가 아닐 때만 표시 */}
+{selectedCtgy !== "하의" && (
+  <>
+    <M.CategoryTitle>소매길이</M.CategoryTitle>
+    <M.CategoryItems>
+      {isEditing
+        ? sleeve.map((item) => (
+            <M.CategoryItem
+              key={item}
+              selected={selectedSleeve.includes(item)}
+              onClick={() => handleSleeveClick(item)}
+            >
+              {item}
+            </M.CategoryItem>
+          ))
+        : selectedSleeve.map((item) => (
+            <M.CategoryItem key={item} selected>
+              {item}
+            </M.CategoryItem>
+          ))}
+    </M.CategoryItems>
+  </>
+)}
+
+  <M.CategoryTitle>기장</M.CategoryTitle>
+<M.CategoryItems>
+  {(() => {
+    const { options, selected } = getLengthOptions();
+    return isEditing
+      ? options.map((item) => (
           <M.CategoryItem
             key={item}
-            selected={selectedNeckline.includes(item)}
-            onClick={() => handleNecklineClick(item)}
+            selected={selected.includes(item)}
+            onClick={() => handleLengthClickDynamic(item)}
           >
             {item}
           </M.CategoryItem>
         ))
-      : selectedNeckline.map((item) => (
+      : selected.map((item) => (
           <M.CategoryItem key={item} selected>
             {item}
           </M.CategoryItem>
-        ))}
-  </M.CategoryItems>
-  <M.CategoryTitle>소매길이</M.CategoryTitle>
-  <M.CategoryItems>
-    {isEditing
-      ? sleeve.map((item) => (
-          <M.CategoryItem
-            key={item}
-            selected={selectedSleeve.includes(item)}
-            onClick={() => handleSleeveClick(item)}
-          >
-            {item}
-          </M.CategoryItem>
-        ))
-      : selectedSleeve.map((item) => (
-          <M.CategoryItem key={item} selected>
-            {item}
-          </M.CategoryItem>
-        ))}
-  </M.CategoryItems>
-  <M.CategoryTitle>길이</M.CategoryTitle>
-  <M.CategoryItems>
-    {isEditing
-      ? length.map((item) => (
-          <M.CategoryItem
-            key={item}
-            selected={selectedLength.includes(item)}
-            onClick={() => handleLengthClick(item)}
-          >
-            {item}
-          </M.CategoryItem>
-        ))
-      : selectedLength.map((item) => (
-          <M.CategoryItem key={item} selected>
-            {item}
-          </M.CategoryItem>
-        ))}
-  </M.CategoryItems>
-  <M.CategoryTitle>코디</M.CategoryTitle>
-  <M.CategoryTitle>구매날짜</M.CategoryTitle>
-  <M.CategoryItems>
-    <input 
-      type="text" 
-      value={purchaseDate} 
-      onChange={handleDateChange} 
-      placeholder="YYYY-MM-DD 형식으로 입력하세요" 
-      disabled={!isEditing} 
-    />
-  </M.CategoryItems>
+        ));
+  })()}
+</M.CategoryItems>
+
+
 </M.CategorySection>
     <M.ButtonContainer>
         <M.EditButton onClick={handleEditClick}>
